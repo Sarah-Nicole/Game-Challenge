eafecc3a0e6aca3886235e013f05b7a49bf2293a
Sarah Kolb <sarah.n.kolb@gmail.com>
2023-02-06 21:58:31 +0100

    add sound to selected btn

--- script.js
+++ script.js
@@ -3,13 +3,8 @@ const buttonColours = ["red", "blue", "green", "yellow"]
 const gamePattern = [];
 
 
-const blueSound = new Audio ("sounds/blue.mp3"); 
-const greenSound = new Audio ("sounds/green.mp3"); 
-const redSound = new Audio ("sounds/red.mp3"); 
-const yellowSound = new Audio ("sounds/yellow.mp3"); 
-const wrongSound = new Audio ("sounds/wrong.mp3"); 
-
-
+ 
+let wrongSound = new Audio ("sounds/wrong.mp3"); 
 
 function nextSequence(){
    let randomNumber = Math.floor(Math.random()*4);
@@ -18,7 +13,8 @@ function nextSequence(){
    //animate the button selected
    $("#" + randomChosenColor).fadeOut(60).fadeIn(60);
 
-   let chosenSound = randomChosenColor + "Sound"; 
+   //play the sound of the button selected
+   let chosenSound = new Audio ("sounds/" + randomChosenColor + ".mp3");
    chosenSound.play(); 
 
 }
