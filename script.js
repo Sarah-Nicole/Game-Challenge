09433dc12b8199be125f5766ea3fb464e118c17a
Sarah Kolb <sarah.n.kolb@gmail.com>
2023-02-10 20:11:20 +0100

    finalize game rules

--- script.js
+++ script.js
@@ -1,21 +1,103 @@
 
 const buttonColours = ["red", "blue", "green", "yellow"]
-const gamePattern = [];
+let gamePattern = [];
+let userClickedPattern = [];
+let gameStarted = false;
+let level = 0; 
 
 
  
-let wrongSound = new Audio ("sounds/wrong.mp3"); 
+
+// ---------- Game rules --------------
+
+function checkAnswer(currentLevel){
+   if (gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+     
+      if(userClickedPattern.length === gamePattern.length){
+         setTimeout(function() {
+            nextSequence();
+         }, 1500);
+         userClickedPattern =[]
+      }
+   } else {
+      // sound wrong
+      let wrongSound = new Audio ("sounds/wrong.mp3"); 
+      wrongSound.play();
+      // red background
+      $("body").addClass("game-over");
+      setTimeout(function(){
+         $("body").removeClass("game-over");
+      }, 200);
+      // changed title
+      $("h1").text("Game Over, press any key to restart.");
+      // preparations to start over
+      startOver();
+   }
+}
 
 function nextSequence(){
    let randomNumber = Math.floor(Math.random()*4);
    let randomChosenColor = buttonColours[randomNumber]; 
    gamePattern.push(randomChosenColor);
-   //animate the button selected
-   $("#" + randomChosenColor).fadeOut(60).fadeIn(60);
 
-   //play the sound of the button selected
-   let chosenSound = new Audio ("sounds/" + randomChosenColor + ".mp3");
-   chosenSound.play(); 
+   animatePress(randomChosenColor);
+   playSound(randomChosenColor);
 
+   //increase the level in title
+   level ++; 
+   $("h1").text("Level " + level); 
 }
+
+function startOver(){
+   level = 0; 
+   gamePattern = []; 
+   gameStarted = false; 
+   userClickedPattern = [];
+}
+
+// --------------  Animation Rules -----------------
+
+//play the sound of the button selected
+function playSound(name){
+   let chosenSound = new Audio ("sounds/" + name + ".mp3");
+   chosenSound.play(); 
+}
+
+//animate the button selected
+function animatePress(currentColor){
+   let pressedBtn = $("#" + currentColor); 
+   pressedBtn.addClass("pressed");
+   setTimeout(function() {
+      pressedBtn.removeClass("pressed");
+   }, 100);
+}
+
+
+
+// --------------- User Imputs ---------------
+
+// keypress to start the game
+$(document).on("keydown", function (){
+
+   if (gameStarted !== true){
+      $("h1").text("Level " + level); 
+      nextSequence();
+      gameStarted = true;
+   }
+   });
+
+
+// button clicked by the user 
+$(".btn").on("click", function(){
+   let userChosenColor = $(this).attr("id"); 
+   userClickedPattern.push(userChosenColor); 
+
+   animatePress(userChosenColor); 
+   playSound(userChosenColor);
+   checkAnswer(userClickedPattern.length -1);
+
+});
+
+
+
 
